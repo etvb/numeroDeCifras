d3d86b3743d6c834ded264b828b7e1ab2e67b1c0
Edgardo Torres Vanden Bouch <edgardo.torres@hotmail.com>
2017-09-25 06:26:32 -0500

    archivos base

---
+++ PATS-23-8-NumeroDeCifras/src/pats/pkg23/pkg8/numerodecifras/PATS238NumeroDeCifras.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pats.pkg23.pkg8.numerodecifras;
+
+/**
+ *
+ * @author edgardo
+ */
+public class PATS238NumeroDeCifras {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
